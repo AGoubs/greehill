7017b30d9e79763d219da4a785541e23a9a7cfbc
AGoubs <arnaud@goubier.fr>
2022-12-24 12:56:44 +0100

    Add theme toggler for login

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html :class="{ 'dark': dark }" lang="{{ str_replace('_', '-', app()->getLocale()) }}" x-data="data()">
 
 <head>
   <meta charset="utf-8">
@@ -14,14 +14,13 @@
   <!-- Scripts -->
   <link rel="stylesheet" href="{{ asset('css/app.css') }}">
   <script src="{{ asset('js/app.js') }}" defer></script>
+  <script src="{{ asset('js/init-alpine.js') }}"></script>
 </head>
 
 <body>
-  <div class="">
-    <div class="flex items-center min-h-screen p-6 bg-gray-50 dark:bg-gray-900">
-      <div class="flex-1 h-full max-w-4xl mx-auto overflow-hidden bg-white rounded-lg shadow-xl dark:bg-gray-800">
-        {{ $slot }}
-      </div>
+  <div class="flex items-center min-h-screen p-6 bg-gray-50 dark:bg-gray-900">
+    <div class="flex-1 h-full max-w-4xl mx-auto overflow-hidden bg-white rounded-lg shadow-xl dark:bg-gray-800">
+      {{ $slot }}
     </div>
   </div>
 </body>
